1bcf8b0f6528c9a231632fe87473eb3310be0345
carlosdoki <carlosdoki@gmail.com>
2016-08-16 11:12:24 -0300

    Update solar.sh

--- solar.sh
+++ solar.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-echo "Inicio - $(date)" >> solar.log
 /opt/AppDynamics/Controller/jre/bin/java -jar SolarWinds.jar "$@"
- for i; do 
-    echo $i >> solar.log 
- done
-echo "Fim - $(date)" >> solar.log
